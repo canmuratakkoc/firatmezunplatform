6bc7361f7d766111437ec0064d692b4cc90ffee0
canmuratakkoc <200290301@firat.edu.tr>
2021-12-26 14:56:36 +0300

    dosyalar

--- site/resources/views/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-    </head>
-    <body class="antialiased">
-      Fırat Mezun Platformu
-    </body>
-</html>
